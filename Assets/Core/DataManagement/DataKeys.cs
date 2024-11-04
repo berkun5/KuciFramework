f7970e67df7bba9dc1e1abcedae8a7c7b083628e
Berk Unal <80388989+berkun5@users.noreply.github.com>
2024-11-04 09:45:03 +0100

    Update DataKeys.cs

--- Assets/Core/DataManagement/DataKeys.cs
+++ Assets/Core/DataManagement/DataKeys.cs
@@ -3,27 +3,6 @@ namespace Kuci.RemoteData
     //keys for saving and loading data
     public static class DataKeys
     {
-        public const string ActiveNetworkIPType = "_activeNetworkIPType";
-        public const string CustomIPText = "_customIPText";
-        public const string ActiveOperatorFaceType = "_activeOperatorFaceType";
-        public const string ActiveRobotType = "_activeRobotType";
-        public const string ActiveTaskId = "_activeTaskId";
-        public const string LastCompletedTask = "_lastCompletedTask";
-        public const string EyeNetworkType = "_eyeNetworkType";
-        public const string SelectedSimulation = "_selectedSimulation";
-        public const string UseRelativeArmsMovement = "UseRelativeArmsMovement";
-        public const string UseImmersiveEyeVision = "UseImmersiveEyeVision";
-        public const string BioIKArmsSpeed = "_bioIKArmsSpeed";
-        public const string EyesBrightness = "_eyesBrightness";
-        public const string UIEntityTransformProperties = "_UIEntityTransformProperties";
-        public const string UniformArmSize = "_uniformArmSize";
-        public const string RobotVolume = "_robotVolume";
-        public const string RobotMovementSpeed = "_robotMovementSpeed";
-        public const string AliceLeftArmControlType = "_aliceLeftArmControlType";
-        public const string AliceRightArmControlType = "_aliceRightArmControlType";
-        public const string FaceInOutPlacementMultiplier = "FaceInOutPlacementMultiplier";
-        public const string FaceUpDownPlacementMultiplier = "FaceUpDownPlacementMultiplier";
-        public const string FaceRightLeftPlacementMultiplier = "FaceRightLeftPlacementMultiplier";
         //public const string ExampleDataKey = "ExampleDataKey_";
     }
 }
